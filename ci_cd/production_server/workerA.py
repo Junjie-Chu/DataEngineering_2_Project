5813cafb451226271dd4a397e71171723037246b
PNightOwlY <15970840304@163.com>
2021-05-31 14:30:39 +0000

    add method of loading NN

--- ci_cd/production_server/workerA.py
+++ ci_cd/production_server/workerA.py
@@ -15,6 +15,13 @@ import numpy as np
 from keras.models import model_from_json
 from keras.models import Sequential
 
+import tensorflow as tf
+from tensorflow.keras import datasets, models, layers 
+from tensorflow.keras.layers import Dense, Dropout
+from tensorflow.keras.wrappers.scikit_learn import KerasRegressor
+from tensorflow.keras.models import load_model
+
+
 #the data file name!
 data_file = 'preprosessedData.csv'
 #the model name we load!
@@ -35,6 +42,32 @@ def load_data():
 def load_model():
     loaded_model = joblib.load(model)
     return loaded_model
+
+#Neural Network 
+def DenseModel():
+    model = models.Sequential()
+    model.add(Dense(128,input_dim=15, activation='relu'))
+    model.add(Dropout(0.3))    
+    model.add(Dense(128, activation='relu'))
+    model.add(Dropout(0.4))
+    model.add(Dense(128, activation='relu'))
+    model.add(Dropout(0.4))
+    model.add(Dense(20, activation='relu'))
+    model.add(Dropout(0.2))
+    model.add(Dense(10, activation='relu'))
+    model.add(Dense(1))
+    
+    model.compile(loss='mean_squared_error', optimizer='adam')
+    
+    return model
+
+
+# if the best model is Neraul network
+def load_NN_Model():
+  estimator = KerasRegressor(build_fn=DenseModel, nb_epoch=200, epochs=10, batch_size=25, verbose=True)
+  estimator.model = load_model("NN_model.h5")  
+  return estimator
+
 # Celery configuration
 CELERY_BROKER_URL = 'pyamqp://rabbitmq:rabbitmq@rabbit:5672/'
 CELERY_RESULT_BACKEND = 'rpc://'
@@ -52,11 +85,7 @@ def get_predictions():
     # print ('results[y]:', results['y'])
     for i in range(len(results['y'])):
         print('%s => %f (expected %f)' % (X[i:i+1].values.tolist(), predictions[i], y[i:i+1]))
-        #print(X[i:i+1].values.tolist())
-        #print(predictions[i])
-        #print(y[i:i+1])
         results['predicted'].append(predictions[i].tolist())
-    # print ('results:', results)
     return results
 
 @celery.task
